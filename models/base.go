647818fd30e7542ca9e9769182f62e8bd9518048
MaoWei <maowei@yuanben.org>
2021-07-05 15:36:15 +0800

    feat(models): base model

--- models/base.go
+++ models/base.go
@@ -8,10 +8,14 @@ import (
 )
 
 type Base struct {
-	ID        uint   `json:"-"`
-	CreatedAt int64  `json:"created_at" description:"创建时间"`
-	UpdatedAt int64  `json:"updated_at" description:"更新时间"`
-	UUID      string `json:"uuid" gorm:"type:varchar(36);unique;not null" description:"业务id"`
+	ID        uint  `json:"-"`
+	CreatedAt int64 `json:"created_at" description:"创建时间"`
+	UpdatedAt int64 `json:"updated_at" description:"更新时间"`
+}
+
+type UUIDBase struct {
+	Base
+	UUID string `json:"uuid" gorm:"type:varchar(36);unique;not null" description:"业务id"`
 }
 
 type PageInput struct {
@@ -20,12 +24,12 @@ type PageInput struct {
 	Order    string
 }
 
-func (b *Base) BeforeCreate(tx *gorm.DB) (err error) {
+func (b *UUIDBase) BeforeCreate(tx *gorm.DB) (err error) {
 	b.UUID = uuid.New().String()
 	return
 }
 
-func (b *Base) Count(value interface{}, query interface{}, args ...interface{}) int64 {
+func Count(value interface{}, query interface{}, args ...interface{}) int64 {
 	var count int64
 	tx := db.GetDB()
 	if query != nil {
